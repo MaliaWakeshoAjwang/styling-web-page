e3d5c9ec723be222cdd8d0b517a70eb70e839c12
MaliaWakeshoAjwang <maz.obiri@emory.edu>
2025-05-17 03:30:12 -0400

    added color palette data

---
+++ src/palette.js
@@ -0,0 +1,54 @@
+export const palette = {
+    light: {
+        brand: [
+            { name: "primary", hex: "#1236d8" },
+            { name: "secondary", hex: "#1236d8" },
+            { name: "tertiary", hex: "#1236d8" },
+            { name: "alternate", hex: "#1236d8" },
+        ],
+        utility: [
+            { name: "primary text", hex: "#1236d8" },
+            { name: "secondary text", hex: "#1236d8" },
+            { name: "primary background", hex: "#1236d8" },
+            { name: "secondary background", hex: "#1236d8" },
+        ],
+        accent: [
+            { name: "accent 1", hex: "#1236d8" },
+            { name: "accent 2", hex: "#1236d8" },
+            { name: "accent 3", hex: "#1236d8" },
+            { name: "accent 4", hex: "#1236d8" },
+        ],
+        semantic: [
+            { name: "success", hex: "#1236d8" },
+            { name: "error", hex: "#1236d8" },
+            { name: "warning", hex: "#1236d8" },
+            { name: "info", hex: "#1236d8" },
+        ],
+    },
+    dark: {
+        brand: [
+            { name: "primary", hex: "#1236d8" },
+            { name: "secondary", hex: "#1236d8" },
+            { name: "tertiary", hex: "#1236d8" },
+            { name: "alternate", hex: "#1236d8" },
+        ],
+        utility: [
+            { name: "primary text", hex: "#1236d8" },
+            { name: "secondary text", hex: "#1236d8" },
+            { name: "primary background", hex: "#1236d8" },
+            { name: "secondary background", hex: "#1236d8" },
+        ],
+        accent: [
+            { name: "accent 1", hex: "#1236d8" },
+            { name: "accent 2", hex: "#1236d8" },
+            { name: "accent 3", hex: "#1236d8" },
+            { name: "accent 4", hex: "#1236d8" },
+        ],
+        semantic: [
+            { name: "success", hex: "#1236d8" },
+            { name: "error", hex: "#1236d8" },
+            { name: "warning", hex: "#1236d8" },
+            { name: "info", hex: "#1236d8" },
+        ],
+    },
+};
